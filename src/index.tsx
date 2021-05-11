c03dff86b6c6279718d45ed1a3b7b3471d115913
Werliton Silva <GFT09@localiza.com>
2021-05-11 08:54:08 -0300

    feat: configuring providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store'
+import Routes from './routes'
+import Wrapper from './components/wrapper/Wrapper';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './styles/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Wrapper>
+        <Routes />
+      </Wrapper>
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
