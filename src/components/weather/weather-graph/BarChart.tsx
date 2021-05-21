20953f5aedfad0b9a3cdf2ce9e9e2b1e84f69081
werliton <werlitoncarlos@gmail.com>
2021-05-20 21:43:45 -0300

    feat: using scales

--- src/components/weather/weather-graph/BarChart.tsx
+++ src/components/weather/weather-graph/BarChart.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from "d3";
 import { makeStyles, Theme } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../../store';
 
 interface BarChartProps {
     data: number[],
@@ -15,6 +17,8 @@ const useStyles = makeStyles((theme: Theme) => ({
 }))
 
 const BarChart: React.FC<BarChartProps> = ({ data }) => {
+    const { weatherResults } = useSelector((state:AppState) => state.weathers)
+  
     const classes = useStyles()
 
     const graphRef = useRef(null)
@@ -22,43 +26,47 @@ const BarChart: React.FC<BarChartProps> = ({ data }) => {
     const svgHeight=  300
     const svgWidth = 600
     const barWidth = Math.round(svgWidth / data.length)
-    const barPadding = 10
 
     useEffect(() => {
+        const forecast = weatherResults?.results?.forecast
         const svg = d3
             .select(graphRef.current)
             .attr("width", svgWidth)
             .attr("height", svgHeight)
             .style('background-color', '#e2d0d0')
-
+        
+        const yScale = d3.scaleLinear()
+        .domain([0, d3.max(forecast.map(item => item.max)) as number])
+        .range([0, svgHeight])
+        // render rectangle
         svg.selectAll("rect")
-            .data(data)
+            .data(forecast)
             .enter()
             .append("rect")
             .attr("x", (d,i) => i * 65)
-            .attr("y", (d) => svgHeight - d * 5)
+            .attr("y", (d) => svgHeight - yScale(d.max))
             .attr("width", barWidth/2)
-            .attr("height", (d, i) => d * 10)
+            .attr("height", (d, i) => yScale(d.max))
             .attr("transform", (d, i) => {
                 const translate = [barWidth * 1, 0]
                 return `translate(${translate})`
             })
-            .attr("fill",'#5288AC')
-
+            .attr("fill", (d,i) => d.max < 21 ? '#9e0404' : '#5288AC')
+        // Incluir labels
         svg.selectAll("text")
-            .data(data)
+            .data(forecast)
             .enter()
             .append("text")
-            .text(d => `${d}º`)
+            .text(d => `${d.max}º`)
             .attr("x", (d, i) => i * 65 + (barWidth + 10))
-            .attr("y", (d, i) => (svgHeight - d * 5) - 5)
-            
-    }, [data, barWidth])
+            .attr("y", (d, i) => (svgHeight - d.max * 5) - 5)
+
+    }, [data, barWidth, weatherResults])
 
     return (
-        <div id="graph" className={classes.container}>
+        // <div id="graph" className={classes.container}>
             <svg ref={graphRef}></svg>
-        </div>
+        // </div>
     )
 }
 
