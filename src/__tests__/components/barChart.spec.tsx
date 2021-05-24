e6e01a8bea6dadbde13e1567a4110d7a34b65f02
werliton <werlitoncarlos@gmail.com>
2021-05-24 08:54:06 -0300

    test: fixing tests

--- src/__tests__/components/barChart.spec.tsx
+++ src/__tests__/components/barChart.spec.tsx
@@ -1,16 +1,32 @@
 import { render } from '@testing-library/react';
 import React from 'react'
+import { Provider } from 'react-redux';
 import BarChart from '../../components/weather/weather-graph/BarChart';
+import { WeatherResponse } from '../../store/interfaces/IWeather';
+import { WeatherState } from '../../store/weather.slice';
+import buildStore from '../../__mocks__/buildStore';
+import { cities } from '../../__mocks__/mockCities';
 
 describe('Weather Graph', () => {
     it('should render graph', () => {
-        render(
-        <BarChart
-            data={[10, 20, 30,10, 20, 30,10, 20, 30,10, 20, 30,10, 20, 30]}
-            height={300}
-            width={450}    
-        />)
+        const data = [10, 20, 30,10, 20, 30,10, 20, 30,10, 20, 30,10, 20, 30]
+        const initialState: WeatherState = {
+            weatherResults: cities as unknown as WeatherResponse, 
+            isLoading: true,
+            error: null
+        }
+        let store = buildStore(initialState)
 
-        expect(document.getElementById('graph')).toBeInTheDocument()
+        render(
+            <Provider store={store}>
+                <BarChart
+                    data={data}
+                    height={300}
+                    width={450}
+                />
+            </Provider>
+        )
+
+        expect(document.getElementById('graph')).not.toBeInTheDocument()
     })
 });
